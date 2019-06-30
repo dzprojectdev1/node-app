037bb58ca8a67cda96456fa98299e7fb79302760
wooyuncr <woo.yun@chromeriver.com>
2019-06-30 00:37:57 -0700

    Latest fix

--- app.js
+++ app.js
@@ -21,9 +21,4 @@ app.use('/api/user', userApi);
 app.use('/api/video', videoApi);
 app.use('/api/match', matchApi);
  
-// set port
-app.listen(8080, function () {
-    console.log('Node app is running on port 3000');
-});
- 
 module.exports = app;
