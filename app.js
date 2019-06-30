44f151b54962677d0912a2fdf6a488c6cb6cbbe4
Vampire <vampire@Vampires-iMac.local>
2019-06-30 00:03:55 -0700

    yang branch test

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+///yang_branch test
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
