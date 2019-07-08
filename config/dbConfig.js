31db8afebf5587a004977e6bed015efdf52f721a
wooyuncr <woo.yun@chromeriver.com>
2019-07-07 23:17:39 -0700

    Configure db connection to use env host var

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 // connection configurations
 var dbConn = mysql.createConnection({
-    host: 'localhost',
+    host: process.env.host || 'localhost',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: 'dzproject'
