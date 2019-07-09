ac56e47027b7f561d279d981b4feb51b28a7e7af
wooyuncr <woo.yun@chromeriver.com>
2019-07-09 11:49:23 -0700

    Add /uploadCredentials GET endpoint

--- routes/storageApi.js
+++ routes/storageApi.js
@@ -2,13 +2,9 @@ const express = require("express");
 const storageApi = express.Router();
 const dbConn = require("../config/dbConfig");
 const checkAuth = require('../middleware/check_auth');
-const {Storage} = require('@google-cloud/storage');
-
-const storage = new Storage({
-  projectId: 'dazzled-date-246123',
-  keyFilename: 'creds.json',
-});
-const bucket = storage.bucket('fireblast-begonia-maxwell-dev');
+const { bucket } = require('../config/storageConfig');
+const uuidv1 = require('uuid/v1');
+const moment = require('moment');
 
 storageApi.get('/videoLink', checkAuth, (req, res) => {
   const fileId = req.query.fileId;
@@ -29,12 +25,37 @@ storageApi.get('/videoLink', checkAuth, (req, res) => {
           if (err) {
             res.status(500).send('Storage API could not get signed URL.');
           } else {
-            res.send(url);
+            res.send({ url });
           }
         });
       } else {
         res.status(400).send('Video not found.');
       }
+    }
+  });
+});
+
+storageApi.get('/uploadCredentials', checkAuth, (req, res) => {
+  const fileId = uuidv1();
+  const file = bucket.file(fileId);
+  const options = {
+    equals: ['$Content-Type', 'video/mp4'],
+    expires: moment().add(1, 'weeks').format('MM-DD-YYYY'),
+    contentLengthRange: {
+      min: 0,
+      // 100MB
+      max: 1024 * 1000 * 100,
+    },
+  };
+
+  file.getSignedPolicy(options, function(err, policy) {
+    if (err) {
+      res.status(500).send('Storage API could not get signed policy.');
+    } else {
+      res.send({
+        policy,
+        fileId,
+      });
     }
   });
 });
