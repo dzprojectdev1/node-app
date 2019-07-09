d16e48f42321cca4ebc711629140f996e4647321
wooyuncr <woo.yun@chromeriver.com>
2019-07-09 15:16:50 -0700

    Add authorization as allowed header

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
 app.use(expressip().getIpInfoMiddleware);
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
     next();
 });
 
