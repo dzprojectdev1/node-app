5345955a04a924d6306519c7b3a4573b06dfd1b8
Vampire <yangdi0027@outlook.com>
2019-09-04 00:20:04 -0700

    check authentication api update

--- middleware/check_auth.js
+++ middleware/check_auth.js
@@ -7,8 +7,6 @@ module.exports = (req, res, next) => {
         req.userData = decode;
         next();
     } catch (error) {
-        return res.status(401).json({
-            message: 'Auth Failed'
-        });
+        return res.status(401).json({ error: true, message: 'Auth Failed' });
     }
 }
