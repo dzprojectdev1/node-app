380b93f32acf41bde183bf698b3f3470e20f23b2
wooyuncr <woo.yun@chromeriver.com>
2019-10-20 14:20:19 -0700

    Configure to use dev cloud sql instance

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,15 +1,18 @@
 var mysql = require('mysql');
 
+var mysqlConfig = {
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  charset: process.env.CHARSET,
+};
+
+if (process.env.CLOUD_SQL_CONNECTION_NAME) {
+  console.log(`This is App Engine. Proceeding to configure mysql with socketPath "/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}".`)
+  mysqlConfig.socketPath = `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`;
+}
 // connection configurations
-var dbConn = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    charset: process.env.CHARSET,
-});
-  
-// connect to database
-dbConn.connect(); 
+var dbConn = mysql.createPool(mysqlConfig);
 
 module.exports = dbConn;
