983159bfcb643cecf84f2809952e2d806a3b8841
meanking <wolf.morgan@outlook.com>
2019-11-01 04:36:40 +0700

    --8

--- routes/matchApi.js
+++ routes/matchApi.js
@@ -968,7 +968,7 @@ matchApi.post('/getAllDiscovers', checkAuth, function (req, res) {
         var joinQuery = ' INNER JOIN tbl_ethnicity AS b ON a.ethnicity_id=b.id INNER JOIN tbl_country AS c ON a.country_id=c.id INNER JOIN tbl_language AS d ON a.language_id=d.id';
 
         if (myLat == 0 && myLong == 0) {
-            var distanceQuery = 'undefined';
+            var distanceQuery = 0;
         } else {
             var distanceQuery = '(3959 * acos (cos(radians(' + myLat + ') ) * cos(radians( a.lat_geo)) * cos(radians(a.long_geo) - radians(' + myLong + ')) + sin (radians(' + myLat + ') ) * sin( radians(a.lat_geo))))';
         }
