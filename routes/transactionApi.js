c648b103b5eec98c4be246e1f94cb71d325743c1
meanking <wolf.morgan@outlook.com>
2019-11-05 02:02:36 +0700

    mass push noti --21

--- routes/transactionApi.js
+++ routes/transactionApi.js
@@ -425,6 +425,7 @@ transactionApi.post('/pushNotification', function(req, res) {
                             
                             return res.send({ error: false, message: "New push notification sent " + num_user + " users." });
                         })
+                        break;
                     }
                 }
             });
