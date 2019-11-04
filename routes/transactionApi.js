3dabc08ec26a4fb683034aa893f46ff26f91d085
meanking <wolf.morgan@outlook.com>
2019-11-05 01:46:53 +0700

    mass push noti --19

--- routes/transactionApi.js
+++ routes/transactionApi.js
@@ -396,17 +396,37 @@ transactionApi.post('/pushNotification', function(req, res) {
         results.map(item => {
 
             i ++;
-            console.log(i);
-            if (i > total_user) {
-                if (success_user > 0) {
+            
+            if (i == total_user) {                
+                var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+                    to: item.fcm_id,
+                    notification: {
+                        title: 'New Notification',
+                        body: messageText,
+                    },
+                    data: {  //you can send only notification or only data(or include both)
+                        type: 'PushNotification',
+                        senderId: senderId,
+                        senderImg: '',
+                        senderName: senderName
+                    }
+                };
+                fcm.send(message, function (notiErr, notiRes) {
+                    if (notiErr) {
+                    } else {
+                        num_user = parseInt(num_user) + 1;
+                    }
+                });
+
+                if (num_user > 0) {
                     message_sent = 1;
                 }
         
                 query = "insert into tbl_message (message, message_sent, num_user, created_date) values (?, ?, ?, ?)";
-                dbConnect.query(query, [messageText, message_sent, success_user, new Date()], function(error, results, fields) {
+                dbConnect.query(query, [messageText, message_sent, num_user, new Date()], function(error, results, fields) {
                     if (error) return res.status(400).send({error: true, detail: error.code, message: error.sqlMessage});
                     
-                    return res.send({ error: false, message: "New push notification sent " + success_user + " users." });
+                    return res.send({ error: false, message: "New push notification sent " + num_user + " users." });
                 })
             } else {
                 var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
