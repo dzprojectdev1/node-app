52f0b91973b0a451afde5b89df1344a4512e2981
Commencement Technology <commencement.technology@gmail.com>
2024-06-30 19:22:13 +0530

    Debugging message issue

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -7,9 +7,10 @@ var mysqlConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  charset: process.env.CHARSET,
 };
 // connectionLimit : 10,
-// charset: process.env.CHARSET,
+
 if (process.env.CLOUD_SQL_CONNECTION_NAME) {
   console.log(`This is App Engine. Proceeding to configure mysql with socketPath "/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}".`)
   mysqlConfig.socketPath = `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`;
